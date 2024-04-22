26c75270bc354a8f59337ac6be465ea2478d7d35
carlos josue lopez  castillo <carlosjlc0216@gmail.com>
2024-04-22 00:06:58 -0600

    solucion cors2?

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
-import express from "express";
+import express from "express"
 import pool from "./config/bd.js";
+import dotenv from "dotenv"
 import routerUsers from "./routes/usuariosRoutes.js";
 import routerQuestion from "./routes/preguntasRoutes.js";
 import routerResult from "./routes/resultadosRoutes.js";
 import fileRoutes from './routes/filesRoutes.js';
 import cors from 'cors';
-
-const app = express();
-
-// Configurar CORS con opciones detalladas
-app.use(cors({
-  origin: '*',
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: 'Content-Type, Authorization',
-}));
-
-app.use(express.json());
+const app = express()
+dotenv.config()
+app.use(express.json())
+app.use(cors())
 
 pool.getConnection()
   .then((connection) => {
@@ -25,14 +19,12 @@ pool.getConnection()
   .catch((err) => {
     console.error('Error al conectarse a la base de datos MySQL:', err.message);
   });
-
-app.use('/uploads', express.static('uploads'));
-app.use('/api/usuarios', routerUsers);
-app.use('/api/preguntas', routerQuestion);
-app.use('/api/resultado', routerResult);
-app.use('/api/files', fileRoutes);
-
+  app.use('/uploads', express.static('uploads'));
+  app.use('/api/usuarios',routerUsers);
+  app.use('/api/preguntas',routerQuestion);
+  app.use('/api/resultado',routerResult);
+  app.use('/api/files', fileRoutes);
 const PORT = 4000;
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
-});
+app.listen(PORT , ()=>{
+    console.log( `server listening on port ${PORT} `);
+} );
