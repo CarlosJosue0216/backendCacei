3a44cb0efd438fc0b9e2f49b4348609b9eef3be8
carlos josue lopez  castillo <carlosjlc0216@gmail.com>
2024-04-21 23:46:17 -0600

    solucion cors?

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-import express from "express"
+import express from "express";
 import pool from "./config/bd.js";
 import routerUsers from "./routes/usuariosRoutes.js";
 import routerQuestion from "./routes/preguntasRoutes.js";
 import routerResult from "./routes/resultadosRoutes.js";
 import fileRoutes from './routes/filesRoutes.js';
 import cors from 'cors';
-const app = express()
-app.use(express.json())
-app.use(cors())
+
+const app = express();
+
+// Configurar CORS antes de definir las rutas
+app.use(cors());
+
+app.use(express.json());
 
 pool.getConnection()
   .then((connection) => {
@@ -17,12 +21,14 @@ pool.getConnection()
   .catch((err) => {
     console.error('Error al conectarse a la base de datos MySQL:', err.message);
   });
-  app.use('/uploads', express.static('uploads'));
-  app.use('/api/usuarios',routerUsers);
-  app.use('/api/preguntas',routerQuestion);
-  app.use('/api/resultado',routerResult);
-  app.use('/api/files', fileRoutes);
+
+app.use('/uploads', express.static('uploads'));
+app.use('/api/usuarios', routerUsers);
+app.use('/api/preguntas', routerQuestion);
+app.use('/api/resultado', routerResult);
+app.use('/api/files', fileRoutes);
+
 const PORT = 4000;
-app.listen(PORT , ()=>{
-    console.log( `server listening on port ${PORT} `);
-} );
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
